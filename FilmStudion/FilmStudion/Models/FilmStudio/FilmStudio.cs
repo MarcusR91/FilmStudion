6904c42753b1b77f8af0779df8cb7ec05bf48b4b
MarcusR91 <Marcus_R91@hotmail.com>
2022-02-05 12:33:06 +0100

    Added models and configured an InMemory DB

---
+++ FilmStudion/FilmStudion/Models/FilmStudio/FilmStudio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmStudion.Models.FilmStudio
+{
+    public class FilmStudio
+    {
+        public int StudioId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Role { get; set; }
+
+    }
+}
---
+++ FilmStudion/FilmStudion/Models/FilmStudio/IFilmStudio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmStudion.Models.FilmStudio
+{
+    interface IFilmStudio
+    {
+        public int StudioId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Role { get; set; }
+    }
+}
